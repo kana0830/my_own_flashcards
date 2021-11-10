2f8eb52aa3d546dde76013d65e0ca3368578e0af
kf0830s <kf0830s@gmail.com>
2021-11-10 18:52:18 +0900

    Add:テスト画面の枠組み追加

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum TestStatus { BEFORE_START, SHOW_QUESTION, SHOE_ANSWER, FINISHED }
+
 class TestScreen extends StatefulWidget {
   final bool isIncludedMemorizedWords;
 
@@ -12,6 +14,44 @@ class TestScreen extends StatefulWidget {
 class _TestScreenState extends State<TestScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("かくにんテスト"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print("Fab押した"), //TODO
+        child: Icon(Icons.skip_next),
+        tooltip: "次にすすむ",
+      ),
+      body: Column(
+        children: [
+          _numberOfQuestionsPart(),
+          _questionOfPart(),
+          _answerCardPart(),
+          _isMemorizedCheckPart(),
+        ],
+      ),
+    );
+  }
+
+  //TODO 残り問題数表示部分
+  Widget _numberOfQuestionsPart() {
+    return Container();
+  }
+
+  //TODO 問題カード表示部分
+  Widget _questionOfPart() {
+    return Container();
+  }
+
+  //TODO こたえカード表示部分
+  Widget _answerCardPart() {
+    return Container();
+  }
+
+  //TODO 暗記済みチェック部分
+  Widget _isMemorizedCheckPart() {
+    return Container();
   }
 }
